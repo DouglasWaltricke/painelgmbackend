b43cbb5cbb3780c1afca8286b7975724c6c10404
Douglas Waltricke <douglas.waltricke@hotmail.com>
2017-08-15 11:52:10 -0300

    teste

---
+++ src/main/java/painelgm/service/UsuarioService.java
@@ -0,0 +1,21 @@
+package painelgm.service;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import painelgm.model.Usuario;
+
+/**
+ *
+ * @author goga
+ */
+@Stateless
+public class UsuarioService {
+    
+    @Inject
+    private EntityManager em;
+    
+    public void cadastrarUsuario(Usuario usuario){
+        em.persist(usuario);
+    }
+    
+}
